6761921a052c6af1357ebdba3174748b736cd294
Jon Bach <bach4@hawaii.edu>
2018-05-02 22:38:07 -1000

    Made tests more specific

--- ziplist.test.js
+++ ziplist.test.js
@@ -5,14 +5,27 @@ describe('ZipList', function () {
   const testList1 = ['a', 'b', 'c'];
   const testList2 = [1, 2, 3];
   describe('ziplist()', function () {
-    it('should return array with all elements of testlist1 and testlist2 added alternatingly', function () {
-      chai.expect(ziplist(testList1, testList2)).to.be.an('array').that.include('a',1,'b',2,'c',3);
+    it('should return single array with six elements', function () {
+      chai.expect(ziplist(testList1, testList2)).to.have.lengthOf(6);
+    });
+    it('should return array with all elements of testlist1 and testlist2', function () {
+      chai.expect(ziplist(testList1, testList2)).to.be.an('array').that.include('a', 'b', 'c', 1, 2, 3);
       // include.members() is used if want to ignore duplicates
+    });
+    it('should deeply equal passed 6 elements in alternation', function () {
+      chai.expect(ziplist(testList1, testList2)).to.eql(['a', 1, 'b', 2, 'c', 3]);
     });
   });
   describe('zipListTheSimpleWay()', function () {
-    it('should return array with all elements of testlist1 and testlist2 added alternatingly', function () {
-      chai.expect(zipListTheSimpleWay(testList1, testList2)).to.be.an('array').that.include('a', 1, 'b', 2, 'c', 3);
+    it('should return single array with six elements', function () {
+      chai.expect(zipListTheSimpleWay(testList1, testList2)).to.have.lengthOf(6);
+    });
+    it('should return array with all elements of testlist1 and testlist2', function () {
+      chai.expect(zipListTheSimpleWay(testList1, testList2)).to.be.an('array').that.include('a', 'b', 'c', 1, 2, 3);
+      // include.members() is used if want to ignore duplicates
+    });
+    it('should deeply equal passed 6 elements in alternation', function () {
+      chai.expect(zipListTheSimpleWay(testList1, testList2)).to.eql(['a', 1, 'b', 2, 'c', 3]);
     });
   });
 });
